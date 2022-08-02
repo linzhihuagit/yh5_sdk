8d6ce2855ec6c402ed897acb43af2b6f53592d1a
lind123 <1397907978@qq.com>
2022-08-02 14:00:11 +0800

    增加banner

--- yh5_sdk/yh5.config.js
+++ yh5_sdk/yh5.config.js
@@ -1,8 +1,9 @@
 window.yh5_config = {
+  app_id: "11",
   more_game_config: [
-    { app_id: "11", name: "gameName", icon: "./yh5_sdk/assets/icon_en.png", url: "11" },
-    { app_id: "11", name: "gameName", icon: "./yh5_sdk/assets/icon_en.png", url: "11" },
-    { app_id: "11", name: "gameName", icon: "./yh5_sdk/assets/icon_en.png", url: "11" },
-    { app_id: "11", name: "gameName", icon: "./yh5_sdk/assets/icon_en.png", url: "11" }
+    { app_id: "11", name: "gameName", icon: "./yh5_sdk/more/icon_en.png", url: "/11", icon_tag: "&icon_tag=1" },
+    { app_id: "11", name: "gameName", icon: "./yh5_sdk/more/icon_en.png", url: "/11", icon_tag: "&icon_tag=1" },
+    { app_id: "11", name: "gameName", icon: "./yh5_sdk/more/icon_en.png", url: "/11", icon_tag: "&icon_tag=1" },
+    { app_id: "11", name: "gameName", icon: "./yh5_sdk/more/icon_en.png", url: "/11", icon_tag: "&icon_tag=1" }
   ]
 }
--- yh5_sdk/yh5.sdk.js
+++ yh5_sdk/yh5.sdk.js
@@ -29,19 +29,58 @@
       document.body.appendChild(document.createRange().createContextualFragment(panelHtml));
     }
     drawElement(data, index) {
-      return `<div class="list-item" onclick="window.open('${data.url}')">
+      return `<a class="list-item" href="${top.location.origin}${data.url}${data.icon_tag}&from_app=${window.yh5.appId}" target="_blank">
         <img src="${data.icon}" alt="" class="game-icon">
         <p class="game-title" style="background-image:url(./yh5_sdk/assets/box_ht_dk${index}.png)">${data.name}</p>
-      </div>`
+      </a>`
+    }
+  }
+  class GameBanner {
+    init(data) {
+      this.isShow = false;
+      this.banner = null;
+      this.listContent = null;
+      this.dataSource = data;
+      this.drawView();
+    }
+    show() {
+      if (!this.banner) {
+        this.banner = document.getElementById("game-banner");
+        this.listContent = document.getElementById("listContent");
+      }
+      this.banner.style.display = "block";
+      this.isShow = true;
+    }
+    hide() {
+      this.banner.style.display = "none";
+      this.isShow = false;
+    }
+    drawView() {
+      var panelHtml = `<div id="game-banner"><div class="bg"></div><div id="listContent" class="container"><div class="content"><div class="list">`;
+      for (let index = 0; index < this.dataSource.length; index++) {
+        panelHtml += this.drawElement(this.dataSource[index], index % 5 + 3);
+      }
+      panelHtml += `</div></div></div></div></div>`;
+      document.body.appendChild(document.createRange().createContextualFragment(panelHtml));
+    }
+    drawElement(data, index) {
+      return `<a class="list-item" href="${top.location.origin}${data.url}${data.icon_tag}&from_app=${window.yh5.appId}" target="_blank">
+        <img src="${data.icon}" alt="" class="game-icon">
+        <p class="game-title" >${data.name}</p>
+      </a>`
     }
   }
   class YH5 {
     constructor() {
+      this.appId = window.yh5_config.app_id;
       this.moreGame = null;
+      this.gameBanner = null;
     }
     initMoreGame() {
       this.moreGame = new MoreGame;
       this.moreGame.init(window.yh5_config.more_game_config);
+      this.gameBanner = new GameBanner;
+      this.gameBanner.init(window.yh5_config.more_game_config);
     }
   }
   window.yh5 = new YH5();
