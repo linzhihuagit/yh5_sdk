25302f2469218bbcdc817461ba4908825f8d7ea2
lind123 <1397907978@qq.com>
2022-08-12 19:16:15 +0800

    sdk修改

--- yh5_sdk/yh5.config.js
+++ yh5_sdk/yh5.config.js
@@ -1,14 +1,14 @@
 window.yh5_config = {
-  app_id: "11",
+  app_id: "191915",
   more_game_config: [
-    { app_id: "11", name: "Crowd Runner", icon: "./yh5_sdk/more/icon_en.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Stickman Legends", icon: "./yh5_sdk/more/stickmanwar_2.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Army Commander", icon: "./yh5_sdk/more/armycommander_1.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "The Hamster Island", icon: "./yh5_sdk/more/hamsterisland_1.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Wooden Carver", icon: "./yh5_sdk/more/woodencarver_1.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Army Commander", icon: "./yh5_sdk/more/armycommander_1.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Stickman War", icon: "./yh5_sdk/more/stickmanwar_1.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Hamster Village", icon: "./yh5_sdk/more/hamsterisland_1.jpg", url: "/11", icon_tag: "&icon_tag=1" },
-    { app_id: "11", name: "Fashion Shoe Collector", icon: "./yh5_sdk/more/icon_en.jpg", url: "/11", icon_tag: "&icon_tag=1" }
+    { app_id: "192704", name: "Crowd Runner", icon: "./yh5_sdk/more/crowdrunner_1.jpg", url: "/games/app/192704?", icon_tag: "icon_tag=crowdrunner_1" },
+    { app_id: "190931", name: "Stickman Legends", icon: "./yh5_sdk/more/stickmanwar_2.jpg", url: "/games/app/190931?", icon_tag: "icon_tag=stickmanwar_2" },
+    { app_id: "192704", name: "Crowd Runner", icon: "./yh5_sdk/more/crowdrunner_3.jpg", url: "/games/app/192704?", icon_tag: "icon_tag=crowdrunner_3" },
+    { app_id: "191572", name: "The Hamster Island", icon: "./yh5_sdk/more/hamsterisland_1.jpg", url: "/games/app/191572?", icon_tag: "icon_tag=hamsterisland_1" },
+    { app_id: "192704", name: "Crowd Runner", icon: "./yh5_sdk/more/crowdrunner_2.jpg", url: "/games/app/192704?", icon_tag: "icon_tag=crowdrunner_2" },
+    { app_id: "190931", name: "Stickman War", icon: "./yh5_sdk/more/stickmanwar_1.jpg", url: "/games/app/190931?", icon_tag: "icon_tag=stickmanwar_1" },
+    { app_id: "190931", name: "Stickman War", icon: "./yh5_sdk/more/stickmanwar_4.jpg", url: "/games/app/190931?", icon_tag: "icon_tag=stickmanwar_4" },
+    { app_id: "191572", name: "Hamster Village", icon: "./yh5_sdk/more/hamsterisland_2.jpg", url: "/games/app/191572?", icon_tag: "icon_tag=hamsterisland_1" },
+    { app_id: "191915", name: "Wooen Carver", icon: "./yh5_sdk/more/woodencarver_1.jpg", url: "/games/app/191915?", icon_tag: "icon_tag=woodencarver_1" }
   ]
 }
--- yh5_sdk/yh5.sdk.js
+++ yh5_sdk/yh5.sdk.js
@@ -36,6 +36,97 @@
       </a>`
     }
   }
+  class MoreGame2 {
+    init(data) {
+      this.dialog = null;
+      this.btnClose = null;
+      this.dataSource = data;
+      this.drawView();
+    }
+    show() {
+      if (!this.dialog) {
+        this.dialog = document.getElementById("more-game2");
+      }
+      if (!window.yh5.canShowAd()) return;
+      this.dialog.style.display = "block";
+    }
+    hide() {
+      this.dialog.style.display = "none";
+    }
+    drawView() {
+      var panelHtml = `<div id="more-game2"><div class="bg"></div><div class="container"><div class="content"><div class="list">`;
+      for (let index = 0; index < this.dataSource.length; index++) {
+        panelHtml += this.drawElement(this.dataSource[index], index % 5 + 3);
+      }
+      panelHtml += `</div></div></div></div></div>`;
+      document.body.appendChild(document.createRange().createContextualFragment(panelHtml));
+    }
+    drawElement(data, index) {
+      return `<a class="list-item" href="${window.yh5.topDomain}${data.url}${data.icon_tag}&from_yapp=${window.yh5.appId}" target="_blank">
+        <img src="${data.icon}" alt="" class="game-icon">
+        <p class="game-title">${data.name}</p>
+      </a>`
+    }
+  }
+  class CustomBox {
+    init(customId, info, data) {
+      this.customId = customId;
+      this.showLen = info ? info.show_len : 3;
+      this.left = info ? info.left : null;
+      this.right = info ? info.right : null;
+      this.top = info ? info.top : null;
+      this.dialog = null;
+      this.customList = null;
+      this.btnClose = null;
+      this.dataSource = data;
+      this.nextIndex = 0;
+      this.drawView();
+    }
+    show() {
+      if (!this.dialog) {
+        this.dialog = document.getElementById("custom-box" + this.customId);
+        this.customList = document.getElementById("custom-list" + this.customId);
+      }
+      if (!window.yh5.canShowAd()) return;
+      this.dialog.style.display = "block";
+      var self = this;
+      this.timeInterval = setInterval(function () {
+        self.updateView();
+      }, 10000);
+    }
+    hide() {
+      this.dialog.style.display = "none";
+      if (this.timeInterval) {
+        clearInterval(this.timeInterval);
+        this.timeInterval = null;
+      }
+    }
+    drawView() {
+      var panelHtml = `<div id="custom-box${this.customId}" class="custom-box" style="${this.left?"left:"+this.left+";":""}${this.right?"right:"+this.right+";":""}${this.top?"top:"+this.top+";":""}"><div class="bg"></div><div class="container"><div class="content"><div id="custom-list${this.customId}" class="list">`;
+      for (let index = 0; index < this.showLen; index++) {
+        this.nextIndex = this.nextIndex % this.dataSource.length;
+        panelHtml += this.drawElement(this.dataSource[this.nextIndex]);
+        this.nextIndex+=1;
+      }
+      panelHtml += `</div></div></div></div></div>`;
+      document.body.appendChild(document.createRange().createContextualFragment(panelHtml));
+    }
+    drawElement(data) {
+      return `<a class="list-item" href="${window.yh5.topDomain}${data.url}${data.icon_tag}&from_yapp=${window.yh5.appId}" target="_blank"><img src="${data.icon}" alt="" class="game-icon"><p class="game-title">${data.name}</p></a>`
+    }
+    updateView() {
+      if (!this.customList) {
+        this.customList = document.getElementById("custom-list" + this.customId);
+      }
+      var panelHtml = "";
+      for (let index = 0; index < this.showLen; index++) {
+        this.nextIndex = this.nextIndex % this.dataSource.length;
+        panelHtml += this.drawElement(this.dataSource[this.nextIndex]);
+        this.nextIndex+=1;
+      }
+      this.customList.innerHTML = panelHtml;
+    }
+  }
   class GameBanner {
     init(data) {
       this.isShow = false;
@@ -57,7 +148,7 @@
         this.bannerList = document.getElementById("bannerList");
       }
       if (!window.yh5.canShowAd() || this.timeInterval) return;
-      
+
       this.banner.style.display = "block";
       this.isShow = true;
       var that = this;
@@ -83,7 +174,7 @@
             }
           }
         }
-      }, 20);
+      }, 30);
       console.log("showBanner");
     }
     hide() {
@@ -116,25 +207,42 @@
   }
   class YH5 {
     constructor() {
-      this.initTime = Math.round((new Date()).getTime()/1000);
+      this.initTime = Math.round((new Date()).getTime() / 1000);
       this.appId = window.yh5_config.app_id;
       this.moreGame = null;
+      this.moreGame2 = null;
       this.gameBanner = null;
+      this.customBox = {};
       this.topDomain = "https://yandex.com";
-      if(location.ancestorOrigins && location.ancestorOrigins.length) {
-        this.topDomain = location.ancestorOrigins[location.ancestorOrigins.length-1];
+      if (location.ancestorOrigins && location.ancestorOrigins.length) {
+        this.topDomain = location.ancestorOrigins[location.ancestorOrigins.length - 1];
       }
     }
     initMoreGame() {
       this.moreGame = new MoreGame;
       this.moreGame.init(window.yh5_config.more_game_config);
     }
+    initMoreGame2() {
+      this.moreGame2 = new MoreGame2;
+      this.moreGame2.init(window.yh5_config.more_game_config);
+    }
     initGameBanner() {
       this.gameBanner = new GameBanner;
       this.gameBanner.init(window.yh5_config.more_game_config);
     }
+    initCustomBox(tag, info) {
+      this.customBox[tag] = new CustomBox;
+      this.customBox[tag].init(tag, info, window.yh5_config.more_game_config)
+    }
+    showCustomBox(tag) {
+      this.customBox[tag].show();
+    }
+    hideCustomBox(tag) {
+      this.customBox[tag].hide();
+    }
     canShowAd() {
-      return Math.round((new Date()).getTime()/1000) - this.initTime > 60;
+      let hostname = location.hostname;
+      return (hostname.indexOf("127.0.0.1") > -1 || hostname.indexOf("localhost")) || Math.round((new Date()).getTime() / 1000) - this.initTime > 60;
     }
   }
   window.yh5 = new YH5();
